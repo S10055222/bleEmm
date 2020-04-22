8fac530e64bd94ce72ca2aade8e17413fc9552d4
s10055222 <s940169@gmail.com>
2020-04-22 14:53:00 +0800

    conter_test_4/22

--- src/app/test/test.page.ts
+++ src/app/test/test.page.ts
@@ -1,32 +1,48 @@
 ﻿import { Component, OnInit, Input } from '@angular/core';
 import { HomePage } from '../../app/home/home.page';
+
 @Component({
     selector: 'app-test',
     templateUrl: 'test.page.html',
     styleUrls: ['test.page.scss']
 })
+
 export class TestPage implements OnInit {
    value = 100;
-
+   pluse = 0;
     firstChange = true;
     lastPrice;
     @Input() price;
     ngOnInit() {
+       
         document.getElementById("value").innerHTML = "100";
+        document.getElementById("pluse").innerHTML = "0";
+        setInterval(() => {
+            this.value += 2;
+            var a = this.value.toString()
+            document.getElementById("value").innerHTML = a;
+
+            this.pluse += 1;
+            var b = this.pluse.toString()
+            document.getElementById("pluse").innerHTML = b;
+        }, 1000);
         
 
-    }
-     increase() {
-        this.value += 2;
-        var a = this.value.toString()
-        document.getElementById("value").innerHTML = a;
 
-    }
+    };
 
-    decrease() {
-        this.value -= 2;
-        var a = this.value.toString()
-        document.getElementById("value").innerHTML = a;
 
-    }
+    // increase() {
+    //    this.value += 2;
+    //    var a = this.value.toString()
+    //    document.getElementById("value").innerHTML = a;
+
+    //}
+
+    //decrease() {
+    //    this.value -= 2;
+    //    var a = this.value.toString()
+    //    document.getElementById("value").innerHTML = a;
+
+    //}
 }
